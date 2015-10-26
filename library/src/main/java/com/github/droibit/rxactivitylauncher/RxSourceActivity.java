ce4f421efe1d3b31cca0dda6dd1321d2702a7b84
Shinya Kumagai <roomful.rooms@gmail.com>
2015-10-27 00:08:07 +0900

    First Commit

---
+++ library/src/main/java/com/github/droibit/rxactivitylauncher/RxSourceActivity.java
@@ -0,0 +1,7 @@
+package com.github.droibit.rxactivitylauncher;
+
+/**
+ * @author kumagai
+ */
+public class RxSourceActivity {
+}
---
+++ sample/src/main/java/com/github/droibit/rxactivitylauncher/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.github.droibit.rxactivitylauncher.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** {@inheritDoc} */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
