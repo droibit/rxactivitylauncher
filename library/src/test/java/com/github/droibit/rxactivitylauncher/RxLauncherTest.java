60dcfe398b6547c3a0e30ddc06a2c96d22463a50
Shinya Kumagai <roomful.rooms@gmail.com>
2015-10-27 19:08:09 +0900

    Added the test class for RxLauncher

---
+++ library/src/test/java/com/github/droibit/rxactivitylauncher/RxLauncherTest.java
@@ -0,0 +1,16 @@
+package com.github.droibit.rxactivitylauncher;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author kumagai
+ */
+public class RxLauncherTest {
+
+    @Test
+    public void lauchFromActivity() {
+
+    }
+}
