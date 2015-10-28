e2cd29a066809df31e7046ffe23c64b25e897280
Shinya Kumagai <roomful.rooms@gmail.com>
2015-10-28 19:23:55 +0900

    Add the sample code where the error occurs
    However, a problem has occurred in the LxLauncher class.

--- sample/src/main/java/com/github/droibit/rxactivitylauncher/app/MainActivity.java
+++ sample/src/main/java/com/github/droibit/rxactivitylauncher/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import com.github.droibit.rxactivitylauncher.ActivityResult;
 import com.github.droibit.rxactivitylauncher.RxLauncher;
 
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 
 import static com.github.droibit.rxactivitylauncher.app.DetailActivity.REQUEST_DETAIL;
@@ -17,6 +18,8 @@ import static com.github.droibit.rxactivitylauncher.app.DetailActivity.REQUEST_D
  * @author kumagai
  */
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_ERROR = 2;
 
     private RxLauncher mLauncher;
 
@@ -45,6 +48,23 @@ public class MainActivity extends AppCompatActivity {
     public void startDaggerActivity(View v) {
         final Intent intent = DaggerActivity.launchIntent(this);
         launchActivity(intent);
+    }
+
+    public void occurActivityNotFondException(View v) {
+        final Intent intent = new Intent("test_action");
+        mLauncher.startActivityForResult(intent, REQUEST_ERROR)
+                 .observeOn(AndroidSchedulers.mainThread())
+                 .doOnError(new Action1<Throwable>() {
+                     @Override
+                     public void call(Throwable throwable) {
+                         Toast.makeText(MainActivity.this, "Error occur : " + throwable.getCause(), Toast.LENGTH_SHORT).show();
+                     }
+                 }).subscribe(new Action1<ActivityResult>() {
+                    @Override public void call(ActivityResult result) {
+                        final String msg = result.isOk() ? "OK" : "Canceled";
+                        Toast.makeText(MainActivity.this, "Received: " + msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void launchActivity(Intent intent) {
