54b4a6d262056388b7684bfb58f420b8bfc8cba3
Shinya Kumagai <roomful.rooms@gmail.com>
2017-01-10 00:15:29 +0900

    Updated LaunchActivitySourceFactory

--- library/src/main/java/com/github/droibit/rxactivitylauncher3/LaunchActivitySourceFactory.java
+++ library/src/main/java/com/github/droibit/rxactivitylauncher3/LaunchActivitySourceFactory.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.RestrictTo;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 
 import io.reactivex.functions.Consumer;
 
@@ -20,12 +21,24 @@ final class LaunchActivitySourceFactory {
     }
 
     @NonNull
-    public static Consumer<Object[]> create(final Activity activity) {
+    static Consumer<Object[]> create(final Activity activity) {
         requireNonNull(activity, "activity must be not null.");
         return new Consumer<Object[]>() {
             @Override
             public void accept(Object[] objects) throws Exception {
-                ActivityCompat.startActivityForResult(activity, ((Intent) objects[0]), ((int) objects[1]), ((Bundle) objects[2]));
+                ActivityCompat.startActivityForResult(activity, ((Intent) objects[0]), ((int) objects[1]),
+                        ((Bundle) objects[2]));
+            }
+        };
+    }
+
+    @NonNull
+    static Consumer<Object[]> create(final Fragment fragment) {
+        requireNonNull(fragment, "fragment must be not null.");
+        return new Consumer<Object[]>() {
+            @Override
+            public void accept(Object[] objects) throws Exception {
+                fragment.startActivityForResult(((Intent) objects[0]), ((int) objects[1]), ((Bundle) objects[2]));
             }
         };
     }
